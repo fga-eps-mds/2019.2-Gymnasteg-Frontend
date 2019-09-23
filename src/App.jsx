1ae45fe22d376dc70ab50fd9f8077c7826541239
Fabricio Santos <fabriciosantos47@gmail.com>
2019-09-23 00:09:05 -0300

    chore: adjust of the front-end url paths so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Page from './Views/Dashboard/Page';
+import Home from './Views/Dashboard/Admin/Home';
 import CadastroBancas from './Views/Dashboard/Admin/CadastroBancas';
+import CadastroArbitros from './Views/Dashboard/Admin/CadastroArbitros';
 import Login from './Views/Login';
 
 import './App.css';
@@ -10,7 +12,11 @@ import './App.css';
 function Pages() {
   return (
     <Page>
-      <Route path="/cadastro/bancas" component={CadastroBancas} />
+      <Switch>
+        <Route path="/cadastro/home" component={Home} />
+        <Route path="/cadastro/bancas" component={CadastroBancas} />
+        <Route path="/cadastro/arbitros" component={CadastroArbitros} />
+      </Switch>
     </Page>
   );
 }
@@ -19,7 +25,7 @@ function App() {
   return (
     <BrowserRouter>
       <Route path="/cadastro" component={Pages} />
-      <Route path="/" exact component={Login} />
+      <Route path="/login" exact component={Login} />
     </BrowserRouter>
   );
 }
