3e20cdebc5f02c8a84d3b8599861f5483a1a0466
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-09-25 00:10:09 -0300

    Created athlete registration page
    Co-authored-by: Alan Marques Sousa <alan.sousa@protonmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Page from './Views/Dashboard/Page';
 import CadastroBancas from './Views/Dashboard/Admin/CadastroBancas';
 import Login from './Views/Login';
+import CadastroAtletaForm from './Views/Dashboard/Admin/CadastroAtletaForm';
 
 import './App.css';
 
 function Pages() {
   return (
     <Page>
+      <Route path="/cadastro/atletas/form" component={CadastroAtletaForm} />
       <Route path="/cadastro/bancas" component={CadastroBancas} />
     </Page>
   );
---
+++ src/Views/Dashboard/Admin/CadastroAtletaForm/CadastroAtletaForm.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Typography, Divider, Row, Button } from 'antd';
+import { Field } from 'formik';
+import PropTypes from 'prop-types';
+import Input from '../../../../Components/DataEntry/Input';
+import DatePicker from '../../../../Components/DataEntry/DatePicker';
+import Select from '../../../../Components/DataEntry/Select';
+import { Container, Col, SubmitHolder } from './CadastroAtletaForm.styles';
+
+const { Title } = Typography;
+
+export default function CadastroAtletaForm({
+  isSubmitting,
+  isValid,
+  handleSubmit,
+}) {
+  return (
+    <Container>
+      <Title level={2}>Cadastro de Atletas</Title>
+      <Divider />
+      <Row>
+        <Col xs={24} md={6}>
+          <Field name="email" label="Email" component={Input} />
+        </Col>
+        <Col xs={24} md={6}>
+          <Field name="nome" label="Nome" component={Input} />
+        </Col>
+        <Col xs={24} md={6}>
+          <Field
+            name="data_nascimento"
+            label="Data de Nascimento"
+            component={DatePicker}
+          />
+        </Col>
+        <Col xs={24} md={6}>
+          <Field
+            name="sexo"
+            label="Sexo"
+            component={Select}
+            data={[
+              { value: 'M', label: 'Masculino' },
+              { value: 'F', label: 'Feminino' },
+            ]}
+          />
+        </Col>
+      </Row>
+      <Divider />
+      <SubmitHolder>
+        <Button
+          type="primary"
+          disabled={!isValid || isSubmitting}
+          onClick={handleSubmit}
+        >
+          Cadastrar Atleta
+        </Button>
+      </SubmitHolder>
+    </Container>
+  );
+}
+
+CadastroAtletaForm.propTypes = {
+  isValid: PropTypes.bool.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
