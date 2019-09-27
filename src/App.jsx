9b17611d85f58ae04f1abb22463a912d522e3b11
Fabricio Santos <fabriciosantos47@gmail.com>
2019-09-27 01:04:20 -0300

    Separate routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import Page from './Views/Dashboard/Page';
+import Home from './Views/Dashboard/Admin/Home';
 import CadastroBancas from './Views/Dashboard/Admin/CadastroBancas';
 import Login from './Views/Login';
 
 import './App.css';
 
-function Pages() {
+function AdminPages() {
   return (
     <Page>
-      <Route path="/cadastro/bancas" component={CadastroBancas} />
+      <Switch>
+        <Route path="/cadastro/home" component={Home} />
+        <Route path="/cadastro/bancas" component={CadastroBancas} />
+        <Redirect to="/cadastro/home" />
+      </Switch>
     </Page>
   );
 }
@@ -18,8 +23,8 @@ function Pages() {
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/cadastro" component={Pages} />
       <Route path="/" exact component={Login} />
+      <Route path="/cadastro" component={AdminPages} />
     </BrowserRouter>
   );
 }
