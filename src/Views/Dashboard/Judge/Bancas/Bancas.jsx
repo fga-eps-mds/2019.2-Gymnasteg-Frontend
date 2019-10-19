2c797526a2b2b7426822dfafe359f287ee4e1230
Alan Marques Sousa <alan.sousa@protonmail.com>
2019-10-19 02:49:18 -0300

    List of referees' stands by day with their respective athletes
    Co-authored-by: Matheus Felizola <matheusbsb2009@hotmail.com>

--- src/Views/Dashboard/Judge/Bancas/Bancas.jsx
+++ src/Views/Dashboard/Judge/Bancas/Bancas.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Collapse, List, Button, Icon, Tabs } from 'antd';
 import { faVoteYea, faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment from 'moment';
 
+import api from '../../../../Services/api';
 import PageContent from '../../../../Components/Layout/PageContent';
 import closestDateToToday from './closestDateToToday';
 
@@ -12,97 +14,118 @@ import './Bancas.css';
 const { Panel } = Collapse;
 
 export default function Bancas() {
-  const dates = ['03/10/2019', '04/10/2019', '05/10/2019'];
+  const [judge, setJudge] = useState([]);
+
+
+  useEffect(() => {
+    async function loadJudge() {
+      const response = await api.get('/judges/2');
+      setJudge(response.data);
+    }
+
+    loadJudge();
+  }, []);
+
+  const [standsByDate, setStandsByDate] = useState({});
+
+  useEffect(() => {
+    const newStandsByDate = {};
+    (judge.stands || []).forEach((stand) => {
+      if (!(stand.date_event in newStandsByDate)) {
+        newStandsByDate[stand.date_event] = [];
+      }
+      newStandsByDate[stand.date_event].push(stand);
+    });
+    setStandsByDate(newStandsByDate);
+  }, [judge]);
 
   return (
     <PageContent title="Bancas" shouldHideDivider>
       <Tabs
-        defaultActiveKey={closestDateToToday(dates)}
+        // defaultActiveKey={closestDateToToday(dates)}
         className="stands-page__tab"
       >
-        {dates.map((date) => (
+        {Object.keys(standsByDate).map((date) => (
           <Tabs.TabPane
-            tab={date
-              .split('/')
-              .slice(0, 2)
-              .join('/')}
+            tab={moment(date).format('DD/MM')}
             key={date}
           >
             <div className="stands-page__tab-content">
               <h2 className="stands-page__title">Bancas a participar</h2>
-              <Collapse defaultActiveKey="Banca 001">
-                <Panel header="Banca 001 | 12h-14h | Argolas" key="Banca 001">
-                  <div className="stands-panel">
-                    <b>Sexo:</b> Feminino
-                    <br />
-                    <b>Modalidade:</b> Argolas
-                    <br />
-                    <b>Período:</b> Dia <b>{date}</b> de <b>12h-14h</b>
-                    <br />
-                    <b>3 árbitros</b> e <b>3 atletas</b>
-                    <br />
-                    <List
-                      size="small"
-                      bordered
-                      dataSource={[
-                        {
-                          name: 'Talita de Alcântara',
-                          disabled: false,
-                          secondsRemaining: 75,
-                        },
-                        { name: 'Pâmela Almeida', disabled: true },
-                        { name: 'Jéssica Pom', disabled: true },
-                      ]}
-                      renderItem={(item) => (
-                        <List.Item>
-                          <div>
-                            <span>{item.name}</span>
+              {standsByDate[date].map((stand) => (
+                <Collapse
+                  defaultActiveKey={`Banca ${standsByDate[date][0].num_stand}`}
+                >
+                  <Panel
+                    header={`Banca ${stand.num_stand} |
+                    ${stand.horary} | ${stand.modality.type || 'null'}`}
+                    key={`Banca ${stand.num_stand}`}
+                  >
+                    <div className="stands-panel">
+                      <b>Sexo:</b>{' '}
+                      {stand.sex_modality === 'M' ? 'Masculino' : 'Feminino'}
+                      <br />
+                      <b>Modalidade:</b> {stand.modality.type}
+                      <br />
+                      <b>Período:</b> Dia <b>{date}</b> de <b>{stand.horary}</b>
+                      <br />
+                      <b>{stand.qtd_judge} árbitros</b> e
+                      <b>{stand.athletes.length} atletas</b>
+                      <br />
+                      <List
+                        size="small"
+                        bordered
+                        dataSource={stand.athletes}
+                        renderItem={(item) => (
+                          <List.Item>
                             <div>
-                              {// eslint-disable-next-line
-                              !item.disabled &&
-                                !Number.isNaN(item.timeRemaining) && (
-                                  <div className="countdown">
-                                    <FontAwesomeIcon
-                                      className="countdown__icon"
-                                      icon={faStopwatch}
-                                    />
-                                    <b>
-                                      {Math.floor(item.secondsRemaining / 60)}:
-                                      {item.secondsRemaining % 60}
-                                    </b>
-                                  </div>
-                                ) // prettier-ignore
-                              }
-
-                              <Button
-                                type="primary"
-                                size="small"
-                                disabled={item.disabled}
-                              >
-                                <div>
-                                  <Icon
-                                    component={() => (
+                              <span>{item.name}</span>
+                              <div>
+                                {
+                                  !item.disabled
+                                  && !Number.isNaN(item.timeRemaining) && (
+                                    <div className="countdown">
                                       <FontAwesomeIcon
-                                        className={`button__icon ${
-                                          item.disabled
-                                            ? 'button__icon--disabled'
-                                            : ''
-                                        }`}
-                                        icon={faVoteYea}
+                                        className="countdown__icon"
+                                        icon={faStopwatch}
                                       />
-                                    )}
-                                  />
-                                  <span>Votar</span>
-                                </div>
-                              </Button>
+                                      <b>
+                                        {Math.floor(item.secondsRemaining / 60)}
+                                        :{item.secondsRemaining % 60}
+                                      </b>
+                                    </div>
+                                  )
+                                }
+                                <Button
+                                  type="primary"
+                                  size="small"
+                                  disabled={item.disabled}
+                                >
+                                  <div>
+                                    <Icon
+                                      component={() => (
+                                        <FontAwesomeIcon
+                                          className={`button__icon ${
+                                            item.disabled
+                                              ? 'button__icon--disabled'
+                                              : ''
+                                          }`}
+                                          icon={faVoteYea}
+                                        />
+                                      )}
+                                    />
+                                    <span>Votar</span>
+                                  </div>
+                                </Button>
+                              </div>
                             </div>
-                          </div>
-                        </List.Item>
-                      )}
-                    />
-                  </div>
-                </Panel>
-              </Collapse>
+                          </List.Item>
+                        )}
+                      />
+                    </div>
+                  </Panel>
+                </Collapse>
+              ))}
             </div>
           </Tabs.TabPane>
         ))}
