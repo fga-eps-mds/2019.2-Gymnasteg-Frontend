d552cff5b88960e1e27f7e1eb57a2e6803e18c49
fabmont <fabriciosantos47@gmail.com>
2019-10-26 13:51:51 -0300

    Change the token location to localstorage instead cookies
    Co-authored-by: fellipe-araujo <araujo.fellipe@hotmail.com>

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'localhost:3333',
-  headers: { Authentication: `Bearer ${Cookies.get('jwt-token')}` },
+  headers: { Authentication: localStorage.getItem('jwt-token') },
 });
 
 export default api;
