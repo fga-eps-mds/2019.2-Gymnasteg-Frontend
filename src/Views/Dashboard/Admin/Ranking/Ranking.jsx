d6781a598feecd2491f7643ffd04f247325c0aa7
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-10-25 23:00:29 -0300

    Added the base of the ranking page.
    Co-authored-by: fabmont <fabriciosantos47@gmail.com>

---
+++ src/Views/Dashboard/Admin/Ranking/Ranking.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { Table } from 'antd';
+import PropTypes from 'prop-types';
+import tableSchema from './tableSchema';
+import PageContent from '../../../../Components/Layout/PageContent';
+
+export default function Ranking({ bancasCadastradas, getStands }) {
+  useEffect(() => {
+    getStands();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <PageContent title="Ranking">
+      <div>
+        <h1>Bancas</h1>
+      </div>
+      <Table
+        columns={tableSchema}
+        dataSource={bancasCadastradas}
+        pagination={false}
+      />
+    </PageContent>
+  );
+}
+
+Ranking.propTypes = {
+  bancasCadastradas: PropTypes.arrayOf(PropTypes.object).isRequired,
+  getStands: PropTypes.func.isRequired,
+};
---
+++ src/Views/Dashboard/Admin/Ranking/tableSchema.js
@@ -0,0 +1,34 @@
+const column = [
+  {
+    title: 'Número da banca',
+    dataIndex: 'num_stand',
+    key: 'num_stand',
+  },
+  {
+    title: 'Modalidade',
+    dataIndex: 'modality',
+    key: 'modality',
+  },
+  {
+    title: 'Sexo',
+    dataIndex: 'sex',
+    key: 'sex',
+  },
+  {
+    title: 'Quantidade de árbitros',
+    dataIndex: 'qtd_judges',
+    key: 'qtd_judges',
+  },
+  {
+    title: 'Quantidade de atletas',
+    dataIndex: 'qtd_athletes',
+    key: 'qtd_athletes',
+  },
+  {
+    title: 'Ação',
+    dataIndex: 'action',
+    key: 'action',
+  },
+];
+
+export default column;
