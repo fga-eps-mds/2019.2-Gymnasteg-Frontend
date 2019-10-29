79c9cfeea923a6b7d96b7dd92892986a43bd1dcc
Fealps <felipeboccardi@hotmail.com>
2019-10-29 11:52:33 -0300

    Started Linking of Front and backend on 'EditarCoordenador"

--- src/Views/Dashboard/Admin/EditarCoordenador/EditarCoordenador.jsx
+++ src/Views/Dashboard/Admin/EditarCoordenador/EditarCoordenador.jsx
@@ -1,8 +1,71 @@
-import React from 'react';
+import { Field } from 'formik';
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { Typography, Divider, Row, Button } from 'antd';
+import Input from '../../../../Components/DataEntry/Input';
 import PageContent from '../../../../Components/Layout/PageContent';
+import { Container, Col, SubmitHolder } from './EditarCoordenador.styles';
 
-export default function EditarCoordenador() {
+const { Title } = Typography;
+
+export default function EditarCoordenador({
+  isSubmitting,
+  isValid,
+  handleSubmit,
+  mapPropsToValues,
+  coordinator,
+}) {
+  useEffect(() => { mapPropsToValues(); }, []);
   return (
-    <PageContent title="Editar Dados Coordenador" />
+    <PageContent>
+      <Container>
+        <Title level={2}>Editar informações</Title>
+        <Divider />
+        <Row>
+          <Col xs={24} md={6}>
+            <Field
+              name="nome"
+              value={coordinator.name}
+              label="Nome"
+              component={Input}
+            />
+          </Col>
+          <Col xs={24} md={6}>
+            <Field
+              name="email"
+              value={coordinator.email}
+              label="Email"
+              component={Input}
+            />
+          </Col>
+          <Col xs={24} md={6}>
+            <Field name="senha" label="Senha" component={Input} />
+            <Field name="novasenha" label="Nova senha" component={Input} />
+          </Col>
+        </Row>
+        <Divider />
+        <SubmitHolder>
+          <Button
+            type="primary"
+            disabled={!isValid || isSubmitting}
+            onClick={handleSubmit}
+            icon="save"
+          >
+              Salvar
+          </Button>
+        </SubmitHolder>
+      </Container>
+    </PageContent>
   );
 }
+
+EditarCoordenador.propTypes = {
+  coordinator: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.bool.isRequired,
+  }).isRequired,
+  mapPropsToValues: PropTypes.func.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
--- src/Views/Dashboard/Admin/EditarCoordenador/index.js
+++ src/Views/Dashboard/Admin/EditarCoordenador/index.js
@@ -1,3 +1,69 @@
+import { withFormik } from 'formik';
+import { compose, withHandlers, withState } from 'recompose';
+import * as Yup from 'yup';
+import { message } from 'antd';
+import api from '../../../../Services/api';
+
 import EditarCoordenador from './EditarCoordenador';
 
-export default EditarCoordenador;
+export function validationSchema() {
+  return Yup.object().shape({
+    email: Yup.string()
+      .email('E-mail deve ser válido')
+      .nullable()
+      .required('Campo obrigatório'),
+    name: Yup.string()
+      .nullable()
+      .required('Campo obrigatório'),
+    senha: Yup.string()
+      .nullable()
+      .required('Campo obrigatório'),
+    novasenha: Yup.string()
+      .nullable()
+      .required('Campo obrigatório'),
+  });
+}
+
+export function mapPropsToValues(props) {
+  const { setCoordinator } = props;
+  return async () => {
+    const { data } = await api.get('/coordinators');
+    const coordinator = {
+      name: data.name,
+      email: data.email,
+    };
+    setCoordinator(coordinator);
+  };
+}
+
+export async function handleSubmit(values, { resetForm }) {
+  const payload = {
+    email: values.email,
+    name: values.name,
+    senha: values.senha,
+    novasenha: values.novasenha,
+  };
+
+  try {
+    await api.put('/coordinators', payload);
+    message.success('Dados do Coordenador atualizados', 4);
+    resetForm();
+  } catch (error) {
+    message.error(error.response.data.error);
+    resetForm();
+  }
+  setTimeout(() => {
+    window.location.replace('/cadastro/editar-perfil');
+  }, 4000);
+}
+
+export default compose(
+  withState('coordinator', 'setCoordinator', {}),
+  withFormik({
+    validationSchema,
+    handleSubmit,
+  }),
+  withHandlers({
+    mapPropsToValues,
+  }),
+)(EditarCoordenador);
