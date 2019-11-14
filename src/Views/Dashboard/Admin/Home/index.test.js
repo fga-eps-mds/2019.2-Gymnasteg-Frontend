f7f20d5c1cac5a31ee00c02c77c317339a8c1742
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-11-14 18:46:52 -0300

    Testing getModalities and getStands of Home.
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- src/Views/Dashboard/Admin/Home/index.test.js
+++ src/Views/Dashboard/Admin/Home/index.test.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { getModalities } from './index';
+import { message } from 'antd';
+import { getModalities, getStands } from './index';
+import api from '../../../../Services/api';
 
-describe('Tela Cadastro Bancas', () => {
-  const mockProps = {};
-  const wrapper = shallow(<getModalities {...mockProps} />);
-
-  test('Deve estar definida', () => {
+describe('Tela Home', () => {
+  const mockReqGet = jest.spyOn(api, 'get');
+  const mockMessage = jest.spyOn(message, 'error');
+  test('getModalities definition', () => {
+    const mockProps = {};
+    const wrapper = shallow(<getModalities {...mockProps} />);
     expect(wrapper).toBeDefined();
+  });
+
+  test('getModalities', async () => {
+    const mockProps = {
+      setModalidades: jest.fn(),
+    };
+
+    mockReqGet.mockImplementation(() => Promise.resolve(mockProps));
+    await getModalities(mockProps)();
+    expect(mockReqGet).toHaveBeenCalled();
+    expect(mockProps.setModalidades).toHaveBeenCalled();
+
+    mockReqGet.mockImplementation(() => Promise.reject());
+    await getModalities(mockProps)();
+    expect(mockProps.setModalidades).toHaveBeenCalled();
+    expect(mockMessage).toHaveBeenCalledWith(
+      'Não foi possível resgatar as modalidades requisitadas.',
+    );
+  });
+
+  test('getStands', async () => {
+    const mockProps = {
+      setBancasCadastradas: jest.fn(),
+    };
+
+    mockReqGet.mockImplementation(() => Promise.resolve(mockProps));
+    await getStands(mockProps)();
+    expect(mockReqGet).toHaveBeenCalled();
+    expect(mockProps.setBancasCadastradas).toHaveBeenCalled();
+
+    mockReqGet.mockImplementation(() => Promise.reject());
+    await getStands(mockProps)();
+    expect(mockProps.setBancasCadastradas).toHaveBeenCalled();
+    expect(mockMessage).toHaveBeenCalledWith(
+      'Não foi possível resgatar as bancas cadastradas.',
+    );
   });
 });
