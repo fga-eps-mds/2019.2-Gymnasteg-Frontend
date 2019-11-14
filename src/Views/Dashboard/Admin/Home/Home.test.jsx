f026183682e7f82767cacb1494524db122d33c9c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-11-14 00:35:41 -0300

    Testing functions on useEffect.
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- src/Views/Dashboard/Admin/Home/Home.test.jsx
+++ src/Views/Dashboard/Admin/Home/Home.test.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Formik } from 'formik';
 import Home from './Home';
 
 describe('Home page', () => {
+  const mockProps = {
+    modalidades: [],
+    getModalities: jest.fn(),
+    getStands: jest.fn(),
+  };
   test('Home', () => {
-    const mockProps = {};
     const wrapper = shallow(<Home {...mockProps} />);
     expect(wrapper).toBeDefined();
+  });
+
+  test('functions on useEffect', () => {
+    mount(
+      <Formik>
+        <Home {...mockProps} />
+      </Formik>,
+    );
   });
 });
