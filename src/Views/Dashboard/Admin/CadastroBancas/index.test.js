9c8fa42cf569b4bb465537fd3ac20e5d8bcaee1e
Fealps <felipeboccardi@hotmail.com>
2019-11-14 01:22:57 -0300

    Testing index.FetchModalidades of CadastroBancas
    Co-authored-by: Fellipe Araujo <araujo.fellipe@hotmail.com>

--- src/Views/Dashboard/Admin/CadastroBancas/index.test.js
+++ src/Views/Dashboard/Admin/CadastroBancas/index.test.js
@@ -1,85 +1,141 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { message } from 'antd';
 import api from '../../../../Services/api';
-import { createMemoryHistory } from 'history'
-import { 
+import {
   validationSchema,
   fetchEditingData,
-  handleSubmit,
+  /*handleSubmit,*/
   fetchModalidades,
-  fetchArbitros,
-  fetchAtletas,
-  fetchStands,
-
 } from './index';
 
-import { message } from 'antd';
 
 describe('Index Cadastro de Bancas', () => {
-    const mockGoBack = jest.fn();
-    const mockProps = {
-      history: {
-        goBack: mockGoBack,
+  const mockGoBack = jest.fn();
+  const mockProps = {
+    history: {
+      goBack: mockGoBack,
+    },
+    stands: [],
+  };
+  const mockReqGet = jest.spyOn(api, 'get');
+  // const mockReqPut = jest.spyOn(api, 'put');
+  // const mockReqPost = jest.spyOn(api, 'post');
+
+  const mockMessage = jest.spyOn(message, 'error');
+  const wrapper = shallow(<index {...mockProps} />);
+
+
+  test('Deve estar definida', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  test('Test validationSchema', async () => {
+    expect(typeof validationSchema()).toBe('object');
+  });
+
+  test('Test fetchEditingData function', async () => {
+    const mockPropsFetch = {
+      match: {
+        params: {
+          idBanca: '1',
+        },
       },
-      stands: [],
+      setEditingData: jest.fn(),
+      setFieldValue: jest.fn(),
     };
-    const mockReqGet = jest.spyOn(api, "get");
-    const mockReqPut = jest.spyOn(api, "put");
-    const mockReqPost = jest.spyOn(api, "post");
+    mockReqGet.mockImplementation(() => Promise.resolve(mockPropsFetch));
+    await fetchEditingData(mockPropsFetch)();
+    expect(mockReqGet).toHaveBeenCalled();
+    expect(await fetchEditingData(mockPropsFetch)()).toBe(null);
 
-    const mockMessage = jest.spyOn(message, 'error');
-    const wrapper = shallow(<index {...mockProps} />);
+    mockReqGet.mockImplementation(() => Promise.reject());
+    await fetchEditingData(mockPropsFetch)();
+    expect(mockPropsFetch.setEditingData).toHaveBeenCalled();
+    expect(mockMessage).toHaveBeenCalledWith(
+      'Ocorreu um erro ao recuperar informações da banca',
+    );
+  });
 
-  
-    test('Deve estar definida', () => {
-      expect(wrapper).toBeDefined();
-    });
+  test('Test handleSubmit function', async () => {
+    /*
+      const mockValues = {
+          num_stand: '1',
+          sex_modality: 'M',
+          qtd_judge: '1',
+          judges: [],
+          athletes: [],
+          date_event: '2019-01-01',
+          horary: '12:00',
+          fk_modality_id: '1',
+          category_age: 'juvenil',
+      };
 
-    test("Test validationSchema", async () => {
-      expect(typeof validationSchema()).toBe('object');
-    });
-
-    test("Test fetchEditingData function", async () => {
-      const mockProps = {
-        match: { 
+      const mockPropsHandle = {
+        history: {
+          goBack: mockGoBack,
+        },
+        match: {
           params: {
             idBanca: '1',
           },
-         },
-         setEditingData: jest.fn(), 
-         setFieldValue: jest.fn(),
+        },
       };
-      mockReqGet.mockImplementation(() => Promise.resolve(mockProps));
-      await fetchEditingData(mockProps)();
-      expect(mockReqGet).toHaveBeenCalled();
-      expect(await fetchEditingData(mockProps)()).toBe(null);
 
-      mockReqGet.mockImplementation(() => Promise.reject());
-      await fetchEditingData(mockProps)();
-      expect(mockProps.setEditingData).toHaveBeenCalled();
+      const mockPropsWithNoId = {
+        history: {
+          goBack:  mockGoBack,
+        },
+        match: {
+          params: {
+            idBanca: '',
+          },
+        },
+      };
+
+      mockReqPut.mockImplementation(() => Promise.resolve(mockPropsHandle));
+      await handleSubmit(mockValues,mockPropsHandle);
+      expect(mockReqPut).toHaveBeenCalled();
+
+
+      mockReqPost.mockImplementation(() => Promise.resolve(
+        mockPropsWithNoId
+        ));
+      await handleSubmit(mockValues,mockPropsWithNoId);
+      expect(mockReqPost).toHaveBeenCalled();
+
+
+      mockReqPut.mockImplementation(() => Promise.reject(mockPropsHandle));
+      await handleSubmit(mockValues,mockPropsHandle);
       expect(mockMessage).toHaveBeenCalledWith(
-        'Ocorreu um erro ao recuperar informações da banca'
+        'Falha ao cadastrar banca.'
         );
-    });
-  
-    test("Test handleSubmit function", async () => {
-
-    });
-
-    test("Test fetchModalidades", async () => {
-
-    });
-
-  test("Test fetchArbitros", async () => {
-      
+      */
   });
 
-  test("Test fetchAtletas", async () => {
-      
+  test('Test fetchModalidades', async () => {
+    const mockPropsFetch = {
+      setModalidades: jest.fn(),
+    };
+    mockReqGet.mockImplementation(() => Promise.resolve(mockPropsFetch));
+    await fetchModalidades(mockPropsFetch)();
+    expect(mockReqGet).toHaveBeenCalled();
+    expect(mockPropsFetch.setModalidades).toHaveBeenCalled();
+
+    mockReqGet.mockImplementation(() => Promise.reject());
+    await fetchModalidades(mockPropsFetch)();
+    expect(mockPropsFetch.setModalidades).toHaveBeenCalled();
   });
 
-  test("Test fetchStands", async () => {
-      
+  test('Test fetchArbitros', async () => {
+
   });
 
-}); 
+  test('Test fetchAtletas', async () => {
+
+  });
+
+  test('Test fetchStands', async () => {
+
+  });
+});
