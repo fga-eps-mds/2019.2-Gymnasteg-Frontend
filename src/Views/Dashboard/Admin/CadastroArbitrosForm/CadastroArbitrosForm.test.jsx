8dd6fba05c650380e3884e80a25847a1ab594f62
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-11-27 22:07:00 -0300

    Testing CadastroArbitrosForm file.

--- src/Views/Dashboard/Admin/CadastroArbitrosForm/CadastroArbitrosForm.test.jsx
+++ src/Views/Dashboard/Admin/CadastroArbitrosForm/CadastroArbitrosForm.test.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Formik } from 'formik';
 import CadastroArbitrosForm from './CadastroArbitrosForm';
 
 describe('Tela Cadastro de Arbitros via form', () => {
-  const mockPrps = {};
-  const wrapper = shallow(<CadastroArbitrosForm {...mockPrps} />);
-
   it('Deve estar definida', () => {
+    const mockProps = {};
+    const wrapper = shallow(<CadastroArbitrosForm {...mockProps} />);
     expect(wrapper).toBeDefined();
+  });
+
+  test('useEffect', () => {
+    const mockProps = {
+      fetchEditingData: jest.fn(),
+    };
+    mount(
+      <Formik>
+        <CadastroArbitrosForm {...mockProps} />
+      </Formik>,
+    );
+    expect(mockProps.fetchEditingData).toHaveBeenCalled();
   });
 });
--- src/Views/Dashboard/Admin/CadastroArbitrosForm/index.test.js
+++ src/Views/Dashboard/Admin/CadastroArbitrosForm/index.test.js
@@ -1,11 +1,46 @@
-import { mapPropsToValues } from '.';
+import { message } from 'antd';
+import { mapPropsToValues, validationSchema, fetchEditingData } from '.';
+import api from '../../../../Services/api';
 
 describe('CadastroArbitrosForm', () => {
+  const mockReqGet = jest.spyOn(api, 'get');
+  const mockMessageError = jest.spyOn(message, 'error');
+
+  test('validationsSchema', () => {
+    expect(typeof validationSchema()).toBe('object');
+  });
+
   test('mapPropsToValues', () => {
     expect(mapPropsToValues()).toEqual({
       name: '',
       email: '',
       JudgeType: 'Execution and Difficulty',
     });
+  });
+
+  test('Test fetchEditingData function', async () => {
+    const mockPropsFetch = {
+      match: {
+        params: {
+          idArbitro: '1',
+        },
+      },
+      setEditingData: jest.fn(),
+      setFieldValue: jest.fn(),
+    };
+
+    mockReqGet.mockImplementation(() => Promise.resolve(mockPropsFetch));
+    await fetchEditingData(mockPropsFetch)();
+    expect(mockReqGet).toHaveBeenCalled();
+    expect(await fetchEditingData(mockPropsFetch)()).toBe(null);
+
+    mockReqGet.mockImplementation(() => Promise.reject());
+    await fetchEditingData(mockPropsFetch)();
+    expect(mockPropsFetch.setEditingData).toHaveBeenCalled();
+    expect(mockMessageError).toHaveBeenCalledWith(
+      'Ocorreu um error ao recuperar as informações do atleta',
+      1.5,
+    );
+    expect(await fetchEditingData(mockPropsFetch)()).toBe(null);
   });
 });
